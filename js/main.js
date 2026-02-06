13796b7252e7206532f9a9a810fd48e32bda7e01
SPIRITUAL-BLOGS <spiritual_masters@zohomail.in>
2026-02-06 15:27:17 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ const navLinks = document.querySelector('.nav-links');
 toggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+// Slideshow auto change
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 4000);
